61d2154d08a310e37ba655fb7e0c28cec195693b
marcelohenrique15 <marcelohenriquepessoal15@gmail.com>
2025-09-05 19:54:45 -0300

    FEAT: Leitura de temperatura e umidade

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define BAUDRATE 115200   //Taxa da comunicação Serial da ESP (bits por segundo)
 
+#define DHTPIN 4          //Porta do sensor na ESP
+#define DHTMODEL DHT11    //Tipo do DHT(pode ser DHT22)
+
+DHT dht(DHTPIN, DHTMODEL);  //Definição do DHT
+
+void setup() 
+{
+  Serial.begin(BAUDRATE);
+  Serial.println(F("Iniciando leitura de Umidade e Temperatura..."));
+  dht.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  float humidity = dht.readHumidity();          //Leitura da umidade
+  float temperature = dht.readTemperature();    //Leitura da temperatura
+
+  if(isnan(humidity) || isnan(temperature))
+  {
+    Serial.println("ERRO na leitura");          //Tratamento de erro caso não retorne um número
+  }
+
+  else                                          //Print para Script python criar o CSV
+  {
+    Serial.print(humidity);
+    Serial.print(",");
+    Serial.println(temperature);
+  }
+
+  delay(2000);
 }
